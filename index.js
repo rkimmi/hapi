6b4e87a485b94ad4f8f013866e55519762ce039e
Kimmi Rindel <kimmirindel97@gmail..com>
2018-02-21 12:56:01 +1300

    added v16 examples for comparison

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ import Hapi from 'hapi'
 const server = new Hapi.server({ port: 3000 }) 
 // by default uses host
 
+// server.connection({
+//     port: 3000,
+//     host: 'localhost'
+// })
+
 const start = async () => {
    await server.start()
 }
-// callbacks are gone — async / await promises only
+// callbacks are gone — async / await promises onl
+
+// server.start((err) => {
+//     if (err ) {
+//         throw err
+//     }
+//     console.log(`Server started at: ${server.info.url}`)
+// })
 
 server.route({
     method:'GET',
